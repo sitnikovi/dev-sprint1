4c313c7a8b25eaad5e21bffe715f4c2373598ed7
Ilya Sitnikov <sitnikovi@hotmail.com>
2013-03-16 15:54:17 -0400

    My first Python app! Yay!!!

--- my_first_app/my_first_app.py
+++ my_first_app/my_first_app.py
@@ -1,2 +1,9 @@
 # This is whre you can start you python file for your week1 web app
-# Name: 
+# Name: Ilya
+
+prompt = raw_input("What is your name? \n \n")
+if prompt == "Ilya":
+    print "Hello" + " " + prompt
+else:
+    print "You're not the one that I'm looking for, sorry! \n I was looking for Ilya" 
+
