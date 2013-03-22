7598f24010c77adb304485afe9b6ec49c567520b
Ilya Sitnikov <sitnikovi@hotmail.com>
2013-03-21 20:58:33 -0500

    wrapped in a function now

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,10 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Ilya Sitnikov
+
+def sticks_length(stick1_length, stick2_length, stick3_length):
+  if stick1_length == stick2_length + stick3_length:
+     print "We certainly can form a triangle"
+  else:
+    print "We absolutely cannot form a triangle"
+sticks_length(12,1,1)
+ 
